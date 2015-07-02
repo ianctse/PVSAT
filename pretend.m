8107b7cdb9f549d99d5d1ba8b91471002b685ac2
ianctse <ian.c.tse@gmail.com>
2015-07-01 17:51:45 -0700

    Edit file using MATLAB
    Commented code and added some math via MATLAB Editor.

--- pretend.m
+++ pretend.m
@@ -1,2 +1,7 @@
-Another new text file.
-Pretend this is a piece of code.
+% Added comment symbols from MATLAB Editor
+% Another new text file.
+% Pretend this is a piece of code
+% 
+% Script to to something
+x = 1:10;
+y = sqrt(x);
